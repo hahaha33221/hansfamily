f6919a6436d8f39362086badb20eca0427756cee
holeeeeeee12 <hand090399@gmail.com>
2026-08-11 17:10:48 +0900

    Fix express body size limit setting

--- server/server.js
+++ server/server.js
@@ -7,7 +7,8 @@ dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 const dbConfig = {
   host: process.env.DB_HOST,
