be15e6cc965a6a965db1d79bdc1609166ce7e0f2
holeeeeeee12 <hand090399@gmail.com>
2026-08-11 12:06:16 +0900

    Configure Vercel rewrite proxy for HTTPS Mixed Content bypass

--- src/config.js
+++ src/config.js
@@ -1,1 +1,3 @@
-export const API_URL = 'http://localhost:5005';
+const isLocal = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
+export const API_URL = isLocal ? 'http://localhost:5005' : '';
+
